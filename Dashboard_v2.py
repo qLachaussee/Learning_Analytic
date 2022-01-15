c114525915eaf08053425ec1034d523bfc513bb9
qLachaussee <55838774+qLachaussee@users.noreply.github.com>
2022-01-15 20:56:58 +0100

    Update Dashboard_v2.py

--- Dashboard_v2.py
+++ Dashboard_v2.py
@@ -25,7 +25,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 sns.set()
 
 id_student = pickle.load(open( "id_student.p", "rb" ) )
-users = {str(id):"mdp" for id in id_student.unique()}
+users = {str(id):"20/20" for id in id_student.unique()}
 
 @st.experimental_memo(suppress_st_warning=True, show_spinner=False)
 def getOneCourse(dataset_dict, code_module, code_presentation):
@@ -507,11 +507,12 @@ def main():
     
     if graph_to_show == "Description des modules":
         
-        student_vle_filtered_MP = filtre_par_3(dataset_dict["studentVle"], code_module, code_presentation)
-        student_vle_filtered_MP.groupby(['code_module']).mean()["sum_click"].plot(kind='bar', stacked=True, figsize=(12,8))
-        plt.title("Nb de clique moyen par module")
-        plt.ylabel("Nb de clique moyen")
-        st.pyplot()
+        with st.expander("Nombre de clique moyen par module :"):    
+            student_vle_filtered_MP = filtre_par_3(dataset_dict["studentVle"], code_module, code_presentation)
+            student_vle_filtered_MP.groupby(['code_module']).mean()["sum_click"].plot(kind='bar', stacked=True, figsize=(12,8))
+            plt.title("")
+            plt.ylabel("Nb de clique moyen")
+            st.pyplot()
 
         with st.expander("Note moyenne par module selon la date :"):    
             
@@ -578,7 +579,7 @@ def main():
             modeles = ("Ada Boost", "K Voisins", "Arbre")
         
         st.subheader("Choisir un modèle :")
-        st.markdown(f"<h6 style='color:yellow; margin-bottom:-25px;'>Conseil : '{modeles[0]}' présente le meilleur taux de réussite.</h6>", unsafe_allow_html=True)
+        st.markdown(f"<h6 style='color:darkgoldenrod; margin-bottom:-25px;'>Conseil : '{modeles[0]}' présente le meilleur taux de réussite.</h6>", unsafe_allow_html=True)
         model_to_show = st.selectbox("", modeles)
         
         if model_to_show == "Arbre":
@@ -607,24 +608,24 @@ if __name__ == '__main__':
         print("noo")
         with st.form(key='login_form'):
             if "username" not in st.session_state:
-                my_user = st.text_input("Username or e-mail")
-                password = st.text_input("Password", type="password")
-            submit_button = st.form_submit_button(label="Submit")
+                my_user = st.text_input("Identifiant")
+                password = st.text_input("Mot de passe", type="password")
+            submit_button = st.form_submit_button(label="Se connecter")
 
             if submit_button:
                 if my_user.lower() in users and users[my_user.lower()] == password:
-                    st.success("Succesfully logged in! :tada:")
+                    st.success("Connexion réussie! :tada:")
                     st.session_state.logged_in = True
                     st.session_state.key = 'OK'
                     st.session_state.id_student = my_user
-                    with st.spinner("Redirecting to application..."):
+                    with st.spinner("Redirection vers l'application..."):
                         st.session_state.data = pickle.load(open("dataset_dict.p", "rb" ))
                         time.sleep(1)
                         print("okkkkkk")
                         st.experimental_rerun()
                         # st.experimental_rerun()
                 else:
-                    st.error("Invalid User/Password. Try again. :no_entry:")
+                    st.error("Identifiant ou mot de passe invalide. Réessayez :no_entry:")
 
     else:
         main()
