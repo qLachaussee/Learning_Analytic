5678706e80a8f0e1191e82fd72868595c193b02b
qLachaussee <55838774+qLachaussee@users.noreply.github.com>
2022-01-15 20:57:43 +0100

    Update Dashboard_v1.py

--- Dashboard_v1.py
+++ Dashboard_v1.py
@@ -26,7 +26,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 sns.set()
 
 id_student = pickle.load(open( "id_student_petit.p", "rb" ) )
-users = {str(id):"mdp" for id in id_student.unique()}
+users = {str(id):"20/20" for id in id_student.unique()}
 
 if __name__ == '__main__':
     # Initialization
@@ -34,24 +34,24 @@ if __name__ == '__main__':
         print("noo")
         with st.form(key='login_form'):
             if "username" not in st.session_state:
-                my_user = st.text_input("Username or e-mail")
-                password = st.text_input("Password", type="password")
-            submit_button = st.form_submit_button(label="Submit")
+                my_user = st.text_input("Identifiant")
+                password = st.text_input("Mot de passe", type="password")
+            submit_button = st.form_submit_button(label="Se connecter")
 
             if submit_button:
                 if my_user.lower() in users and users[my_user.lower()] == password:
-                    st.success("Succesfully logged in! :tada:")
+                    st.success("Connexion réussie! :tada:")
                     st.session_state.logged_in = True
                     st.session_state.key = 'OK'
                     st.session_state.id_student = my_user
-                    with st.spinner("Redirecting to application..."):
+                    with st.spinner("Redirection vers l'application..."):
                         st.session_state.data = pickle.load(open("dataset_dict_petit.p", "rb" ))
                         time.sleep(1)
                         print("okkkkkk")
                         st.experimental_rerun()
                         # st.experimental_rerun()
                 else:
-                    st.error("Invalid User/Password. Try again. :no_entry:")
+                    st.error("Identifiant ou mot de passe invalide. Réessayez :no_entry:")
 
     else:
         main()
